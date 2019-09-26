58f0c3425f535cf004e101bc907ae60623f4b9af
mahboobehmohammadi <mahboub.meng@gmail.com>
2019-09-26 18:51:38 +0800

    Updates

---
+++ br_journal/js/bot/24-f99bde.bot.js
@@ -0,0 +1,14 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([[24],{
+
+/***/ "./src/scratch/help-content/help-string/ohlc.json":
+/*!********************************************************!*\
+  !*** ./src/scratch/help-content/help-string/ohlc.json ***!
+  \********************************************************/
+/*! exports provided: text, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"text\\\":[\\\"This block returns the list of candles. Each candle has 4 properties: high, low, open, close, and open time.\\\",\\\"Example:\\\",\\\"In this example we extract open prices from a candle list into variable candle_list with the help of another block.\\\",\\\"In the above example it is assumed that variable candle_list is processed somewhere within other blocks.\\\"]}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmcvb2hsYy5qc29uLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-string/ohlc.json\n");
+
+/***/ })
+
+}]);
---
+++ br_journal/js/bot/30-7bfd7c.bot.js
@@ -0,0 +1,14 @@
+(window["webpackJsonpderiv_bot"] = window["webpackJsonpderiv_bot"] || []).push([[30],{
+
+/***/ "./src/scratch/help-content/help-string/read_ohlc.json":
+/*!*************************************************************!*\
+  !*** ./src/scratch/help-content/help-string/read_ohlc.json ***!
+  \*************************************************************/
+/*! exports provided: text, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"text\\\":[\\\"This block returns specified candle value for the selected time interval.\\\",\\\"In the below example an open price is extracted from a candle into variable candle_open_price. The Candle data is provided by another block (In candle list get # from end).\\\"]}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyYXRjaC9oZWxwLWNvbnRlbnQvaGVscC1zdHJpbmcvcmVhZF9vaGxjLmpzb24uanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/scratch/help-content/help-string/read_ohlc.json\n");
+
+/***/ })
+
+}]);
